6dd0b9e5fc896d93768d386d846deb9773ad3680
Protik Acharjay <protik7777777@gmail.com>
2025-01-25 00:56:46 +0600

    post model ready and register in admin

--- GhorChai/RentPost/models.py
+++ GhorChai/RentPost/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Post(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='post_images', blank=True, null=True)
+    price = models.IntegerField()
+    location = models.CharField(max_length=100)
+    contact = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.owner.username} - {self.title}'
