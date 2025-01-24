48ca2906807d082dd0d5c06d426443bf8d3239ee
Protik Acharjay <protik7777777@gmail.com>
2025-01-25 01:23:37 +0600

    form configured and created CRUD operation views non-functional

--- GhorChai/RentPost/views.py
+++ GhorChai/RentPost/views.py
@@ -1,5 +1,47 @@
 from django.shortcuts import render
+from .models import Post
+from .forms import PostForm
+from django.shortcuts import get_object_or_404, redirect
+
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def post_list(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'post_list.html', {'posts': posts})
+
+def post_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save(commit=False)
+            form.owner = request.user
+            form.save()
+            return redirect('post_list')
+    else:
+        form = PostForm()
+    return render(request, 'post_form.html', {'form': form})
+
+def post_edit(request, post_id):
+    post = get_object_or_404(Post, pk=post_id, owner=request.user)
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save(commit=False)
+            form.owner = request.user
+            form.save()
+            return redirect('post_list')
+    else:
+        form = PostForm(instance=post)
+
+    return render(request, 'post_form.html', {'form': form})
+
+
+def post_delete(request, post_id):
+    post = get_object_or_404(Post, pk=post_id, owner=request.user)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('post_list')
+    return render(request, 'post_confirm_delete.html', {'post': post})
