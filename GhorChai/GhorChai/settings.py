d01ef406da46e9c7e1bb4bc972f9f68f01de3bd7
Protik Acharjay <protik7777777@gmail.com>
2025-01-25 00:16:18 +0600

    Configure starting an app and flow

--- GhorChai/GhorChai/settings.py
+++ GhorChai/GhorChai/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'RentPost',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'GhorChai.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ GhorChai/RentPost/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RentpostConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'RentPost'
