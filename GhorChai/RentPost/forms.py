060dc98139ca2aa72cf1c735c2b36b74fdf0ee13
Protik Acharjay <protik7777777@gmail.com>
2025-01-25 23:16:57 +0600

    Created registration

--- GhorChai/RentPost/forms.py
+++ GhorChai/RentPost/forms.py
@@ -1,7 +1,16 @@
 from django import forms
 from .models import Post
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'description', 'image', 'price', 'location', 'contact']
+
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField()
+    class Meta:
+        # for built in model use touple not array
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
