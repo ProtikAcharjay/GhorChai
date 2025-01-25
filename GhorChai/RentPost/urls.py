3177ad33f7fa3a1029ab8c0f75f262217e73928d
Protik Acharjay <protik7777777@gmail.com>
2025-01-25 21:54:42 +0600

    setup urls and and created basic templates

--- GhorChai/RentPost/urls.py
+++ GhorChai/RentPost/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
+    path('', views.post_list, name='post_list'),
+    path('create/', views.post_create, name='post_create'),
+    path('<int:post_id>/edit/', views.post_edit, name='post_edit'),
+    path('<int:post_id>/delete/', views.post_delete, name='post_delete'),
 ]
